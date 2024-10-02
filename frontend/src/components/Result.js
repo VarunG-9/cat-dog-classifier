bd76a7336975f2a9879c2401f17b6640a1741f55
Varun G <varungadde@outlook.com>
2024-10-01 22:47:36 -0400

    Added a better alert and also fixed uploads staying in the Backend

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
 
 export default function Result ({result}) {
 
     return (
         <div id="result-div">
-            <h2 id="result"> Your image is classified as a {result}</h2>
+            <Alert variant="success" id="alert"> Your image was classified as a {result}!</Alert>
+            {/* <h2 id="result"> Your image is classified as a {result}!</h2>*/}
         </div>
     )
 }
